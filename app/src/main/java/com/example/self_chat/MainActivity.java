c585c20191abc25bf5ebf84aa1ff789dfd7f00e5
akiva hassan <akivah01@gmail.com>
2019-04-03 21:03:58 +0300

    added stuff on java

--- app/src/main/java/com/example/self_chat/MainActivity.java
+++ app/src/main/java/com/example/self_chat/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.self_chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView text_1;
+    EditText text_2;
+    Button button_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text_1=(TextView) findViewById(R.id.textView);
+        EditText text_2=(EditText) findViewById(R.id.plain_text_input);
+        Button button_1=(Button) findViewById(R.id.button);
+    }
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.button:
+                text_1.setText(text_2.getText().toString());
+                text_2.setText("");
+                break;
+
+        }
     }
 }
+
