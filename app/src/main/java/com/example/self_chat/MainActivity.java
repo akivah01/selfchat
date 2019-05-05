6d8769d175d882c05588a545d95057664c67f1a5
akiva hassan <akivah01@gmail.com>
2019-05-05 18:28:38 +0300

    ex3

--- app/src/main/java/com/example/self_chat/MainActivity.java
+++ app/src/main/java/com/example/self_chat/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.self_chat;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,57 +15,96 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements MyAdapter.LongClick{
 
     EditText input;
     Button button;
     MyAdapter adapter;
     RecyclerView recyclerView;
 
+    SharedPreferences MySharedPrefrance;
+    SharedPreferences.Editor MyEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         recyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
         input = (EditText) findViewById(R.id.EditText);
         button = (Button) findViewById(R.id.Button);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyAdapter();
+        MySharedPrefrance = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+        int size = MySharedPrefrance.getInt("Datasize", 0);
+        MyEditor = MySharedPrefrance.edit();
+        adapter = new MyAdapter(size , MySharedPrefrance, MyEditor);
+        adapter.setClickListener((MyAdapter.LongClick) this);
         recyclerView.setAdapter(adapter);
+
 
         if (savedInstanceState != null)
         {
-            input.setText(savedInstanceState.getString("input"));
+            String previusMessages = (savedInstanceState.getString("input"));
             adapter.setData(savedInstanceState.getStringArrayList("list"));
+            input.setText(previusMessages);
             adapter.notifyDataSetChanged();
+        } if(size != 0 ) {
+            adapter.updateData();
         }
 
-        button.setOnClickListener(this);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                String massage;
+                massage = input.getText().toString();
+                input.setText("");
+                if (massage.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "EMPTY MASSAGE !"
+                            , Toast.LENGTH_LONG).show();
+                    return;
+                }
+                adapter. messages.add(massage);
+                adapter.DataLength = adapter.DataLength+1;
+                adapter.saveEditions();
+                adapter.notifyDataSetChanged();
 
+            }
+        });
+    }
+
+    @Override
+    public void itemLongClick(View view, final int position) {
+        new AlertDialog.Builder(this)
+                .setTitle("Delete message?")
+                .setMessage("Are you sure?")
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        adapter.messages.remove(position);
+                        adapter. DataLength =  adapter.DataLength-1;
+                        adapter. saveEditions();
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(getApplicationContext(),
+                                "Message deleted", Toast.LENGTH_LONG).show();
+
+                    }
+                })
+                .setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(),"Not deleted",
+                                Toast.LENGTH_LONG).show();
+                    }
+                })
+                .show();
     }
 
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString("input", input.getText().toString());
         outState.putStringArrayList("list", adapter.getData());
-    }
-
-    @Override
-    public void onClick(View view) {
-        String massage;
-        massage = input.getText().toString();
-        input.setText("");
-        if (massage.length() == 0 ){
-            Toast.makeText(getApplicationContext(), "EMPTY MASSAGE !"
-                    , Toast.LENGTH_LONG).show();
-            return;
-        }
-        adapter.messages.add(massage);
-        adapter.notifyDataSetChanged();
-
-
-
     }
 }
--- app/src/main/java/com/example/self_chat/MyAdapter.java
+++ app/src/main/java/com/example/self_chat/MyAdapter.java
@@ -6,45 +6,74 @@ import java.util.ArrayList;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
-
+import android.content.SharedPreferences;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
 
 
 class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+
+
+    ArrayList<String> messages;
+    LongClick click;
+    int DataLength;
+    Gson G_son;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    public  interface  LongClick {
+        void itemLongClick(View view, final int position);
+    }
+
 
     public void setData(ArrayList<String> list) {
         messages = list;
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+
         TextView textView;
-        MyViewHolder(View v) {
+        public MyViewHolder(View v) {
             super(v);
             textView = ((TextView) v.findViewById(R.id.TextView));
+            v.setOnLongClickListener(this);
         }
 
         public void display(String message) {
             textView.setText(message);
         }
 
+        public boolean onLongClick(View v) {
+            if (click != null) {
+                click.itemLongClick(v, getAdapterPosition());
+            }
+            return true;
+        }
     }
 
-    ArrayList<String> messages;
 
-    public MyAdapter(){
+    public MyAdapter( int size, SharedPreferences sp, SharedPreferences.Editor edit){
         messages = new ArrayList<String>();
+        G_son = new Gson();
+        DataLength = size;
+        sharedPreferences = sp;
+        editor = edit;
     }
+
 
     @Override
     public MyViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
-
         View v = (View) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.viewsignalmsg, parent, false);
 
-        MyViewHolder viewHolderh = new MyViewHolder(v);
-        return viewHolderh;
-
+        MyViewHolder VH = new MyViewHolder(v);
+        return VH;
 
     }
+
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         holder.display(messages.get(position));
@@ -55,8 +84,26 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return messages.size();
     }
 
+    public void saveEditions()
+    {
+        editor.putInt("Datasize", DataLength);
+        String json = G_son.toJson(messages);
+        editor.putString("sent", json);
+        editor.apply();
+    }
+
+    public void setClickListener(LongClick itemClick) {
+        this.click = itemClick;
+    }
+
     public ArrayList<String> getData() {
         return messages;
     }
 
+    public void updateData( ){
+        String j_son = sharedPreferences.getString("sent", "");
+        Type typeToken = new TypeToken<List<String>>() {
+        }.getType();
+        messages = G_son.fromJson(j_son, typeToken);
+    }
 }
